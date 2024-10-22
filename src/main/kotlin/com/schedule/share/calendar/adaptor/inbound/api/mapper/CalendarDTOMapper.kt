85628e3963be9c3cd5eb22d3bda9929c8f6c6e5f
sonjungchan <rnrmfjc@gmail.com>
2024-10-22 23:54:04 +0900

    feat: calendar 도메인 분리

---
+++ src/main/kotlin/com/schedule/share/calendar/adaptor/inbound/api/mapper/CalendarDTOMapper.kt
@@ -0,0 +1,33 @@
+package com.schedule.share.calendar.adaptor.inbound.api.mapper
+
+import com.schedule.share.calendar.adaptor.inbound.api.dto.CalendarRequestDTO
+import com.schedule.share.calendar.adaptor.inbound.api.dto.CalendarResponseDTO
+import com.schedule.share.calendar.application.service.calendar.vo.CalendarVO
+
+fun CalendarRequestDTO.Calendar.toVO(
+    createdBy: Long,
+    image: ByteArray,
+    userId: Long
+): CalendarVO.Save =
+    CalendarVO.Save(
+        isPublic = isPublic,
+        title = title,
+        content = content,
+        image = image,
+        createdBy = createdBy,
+        userId = userId
+    )
+
+
+fun CalendarVO.Calendar.toResponse(): CalendarResponseDTO.Response =
+    CalendarResponseDTO.Response(
+        id = id!!,
+        isPublic = isPublic,
+        title = title,
+        content = content,
+        image = image,
+        createdBy = createdBy,
+        createdAt = createdAt!!,
+        modifiedAt = modifiedAt!!,
+        isDeleted = isDeleted
+    )
